b95431a6304ed6f1b38fba1737b7bc355d65fa2e
wangluheng328 <57925878+wangluheng328@users.noreply.github.com>
2021-11-16 10:38:04 -0500

    Add corr by month all crime

---
+++ Correlation analysis/all_crime_by_month.scala
@@ -0,0 +1,12 @@
+var crime_date = spark.read.options(Map("inferSchema"->"true", "header"->"true")).csv("/user/lw2534/stage2/crime_date.csv")
+
+var covid = spark.read.options(Map("inferSchema"->"true", "header"->"true")).csv("/user/lw2534/stage2/epid_clean.csv")
+
+covid = covid.filter(covid("location")==="US_NY_NYC")
+
+var covid_month = covid.groupBy(month(col("date")).alias("month")).agg(mean(col("new_confirmed")).alias("mean_new_confirmed"))
+
+var crime_month = crime_date.groupBy(month(col("date")).alias("month")).agg(mean(col("count")).alias("mean_crime_count"))
+
+val df = covid_month.join(crime_month, Seq("month"))
+df.stat.corr("mean_new_confirmed","mean_crime_count")
